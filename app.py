eb8f808c2ac4246985d98127372bb9891c8cdef1
Castiron_Pan <122123502+midnight9902@users.noreply.github.com>
2023-08-13 22:11:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
 my_docs = [
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-7-Summer2023.pdf",
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-8-Spring2023.pdf",
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-9-Spring2023.pdf",
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-1-Summer2023.pdf",
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-2-Summer2023.pdf",
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-3-Summer2023.pdf",
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-4-Summer2023.pdf",
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-5-Summer2023.pdf",
-    r"C:\Eric\School\Advanced Topics in Data Science\Lecture-6-Summer2023.pdf"]
+    r"pdf/Lecture-1-Summer2023.pdf",
+    r"pdf/Lecture-2-Summer2023.pdf",
+    r"pdf/Lecture-3-Summer2023.pdf",
+    r"pdf/Lecture-4-Summer2023.pdf",
+    r"pdf/Lecture-5-Summer2023.pdf",
+    r"pdf/Lecture-6-Summer2023.pdf",
+    r"pdf/Lecture-7-Summer2023.pdf",
+    r"pdf/Lecture-8-Spring2023.pdf",
+    r"pdf/Lecture-9-Spring2023.pdf"]
 docs = Docs()
 for d in my_docs:
     docs.add(d)
