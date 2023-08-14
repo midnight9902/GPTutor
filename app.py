25a21b4dc5abda7b5ecb734fce61161136e3eb12
Castiron_Pan <122123502+midnight9902@users.noreply.github.com>
2023-08-13 23:29:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,7 @@ os.environ.get('OPENAI_API_KEY')
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
-my_docs = [
-    r"pdf/Lecture-1-Summer2023.pdf",
-    r"pdf/Lecture-2-Summer2023.pdf",
-    r"pdf/Lecture-3-Summer2023.pdf",
-    r"pdf/Lecture-4-Summer2023.pdf",
-    r"pdf/Lecture-5-Summer2023.pdf",
-    r"pdf/Lecture-6-Summer2023.pdf",
-    r"pdf/Lecture-7-Summer2023.pdf",
-    r"pdf/Lecture-8-Spring2023.pdf",
-    r"pdf/Lecture-9-Spring2023.pdf"]
+my_docs = generatedcoursedata
 docs = Docs()
 for d in my_docs:
     docs.add(d)
